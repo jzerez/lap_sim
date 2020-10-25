aea597934e08a8b88dcd4a3e85ebe94e49afb9e1
Naomi Chiu <nchiu@olin.edu>
2020-10-25 17:47:16 -0400

    updated curvature based apex detection

--- trackprocessing/convert2graph.m
+++ trackprocessing/convert2graph.m
@@ -3,6 +3,7 @@
 %%
 clear all
 close all
+manual_apex = xlsread('apex.xlsx')*.4;
 color = imread("MichiganTrack2019.jpg");
 grey = rgb2gray(color);
 bw = grey<=125;
@@ -14,14 +15,17 @@ start = [start_y,start_x];              %starting coordinate
 
 boundary = bwtraceboundary(bw,start,'N');       %trace of bw image along boundary 
 %scale = 1/(max(boundary(:,2))/(10.8667*60.96));
-reduced = boundary(1:10:end,:);
+reduced = boundary(1:2:end,:);
 
 points = zeros(size(reduced));
 points(:,1) = reduced(:,2)*scale;
 points(:,2) = (reduced(:,1)*-1+670).*scale;
 
 figure
+hold on
 plot(points(:,1),points(:,2)) %plot track
+plot(manual_apex(:,1),manual_apex(:,2),'rs')
+hold off
 
 diff_points = [points(end,:); points];
 points = points(1:end-1,:);             %remove repeat
@@ -32,22 +36,32 @@ for i = 1:size(diff_points,1)-2
     radius(i) = norm(center(i,:)-diff_points(i+1,:));
 end
 
+% figure
+% hold on
+% plot(center(:,1),center(:,2),'ro')
+% 
+% hold off
 % hold ons
 % ind = better_radius <= 100;
 % plot(points(ind,1),points(ind,2),'ro')
 % hold off
 
-apex = points(radius'<=20,:);
+index_sums = (islocalmin(radius,'MinSeparation',5))+(radius<=25);
+apex_index = index_sums == 2;
+apex = diff_points(apex_index,:);
 zz = zeros(size(points,1),2);
 figure
 hold on
 surf([points(:,1) points(:,1)],[points(:,2) points(:,2)],zz,[radius' radius'],'EdgeColor','interp');
+plot(manual_apex(:,1),manual_apex(:,2),'rs')
 plot(apex(:,1),apex(:,2),'bd')
 %plot(center(:,1),center(:,2),'ro')
 %quiver(diff_points(2:end-1,1),diff_points(2:end-1,2),radius_vector(:,1),radius_vector(:,2),'g-')
 colormap parula
 colorbar
 caxis([0 75])
+xlim([0 700])
+ylim([0 300])
 hold off
 %%
 function center = calc_circle(p1, p2, p3)
--- trackprocessing/grapherboi.m
+++ trackprocessing/grapherboi.m
@@ -2,8 +2,10 @@
 %requires image resolution of 150ppi. Track should be scaled to 1in = 200ft in image. Plots will have a scale ratio of 0.4 m per unit.  
 %%
 clear all
-close all
+%close all
+manual_apex = xlsread('apex.xlsx')*.4;
 track = ~imbinarize(rgb2gray(imread("MichiganTrack2019.jpg")), 0.5);
+ 
 scale = 0.4;
 
 start_x = size(track,2)/2;              % define starting x-coordinate for boundary trace
@@ -13,11 +15,11 @@ start = [start_y,start_x];              % starting coordinate
 boundary = bwtraceboundary(track,start,'N')';       %trace of bw image along boundary 
 %%
 close all
-reduced = boundary(:, 1:2:end);
+reduced = boundary(:,1:4:end);
 
 points = zeros(size(reduced));
 points(1, :) = reduced(2, :)*scale;
-points(2, :) = -reduced(1, :).*scale;
+points(2, :) = (-reduced(1, :)+670).*scale;
 
 figure
 plot(points(1, :),points(2, :)) %plot track
@@ -26,41 +28,45 @@ rs = zeros([1, length(points)]);
 ds = zeros(size(rs));
 angles = zeros(size(rs));
 
-mean_size = 20;
+mean_size = 6;
 
 num_pts = length(diff_points);
 
 for i = 1:num_pts
-    last_ind = mod(i-mean_size/2, num_pts)+1;
-    first_ind = mod(i+mean_size/2, num_pts)+1;
-   
-    v1 = unit(diff_points(:, last_ind));
-    v2 = unit(diff_points(:, first_ind));
+    shifted = circshift(diff_points,i+(mean_size/2)-1,2);
     
-    p1 = points(:, last_ind);
-    p2 = points(:, first_ind);
-   
+    v1 = unit(shifted(:, 1));
+    v2 = unit(shifted(:, mean_size+1));
+    
+    vs = vecnorm(shifted(:, 1:mean_size+1));
+    
     angle = acos(dot(v1, v2));
-    d = norm(p1-p2);
+    d = sum(vs);
 
     angles(i) = real(angle);
     ds(i) = d;
     rs(i) = (real(angle) / d);
 end
 
+rs = fliplr(rs);
+apex = points(:,islocalmax(rs));
+
 zz = zeros([2, length(points)]);
 figure
 hold on
 h = surf([points(1,:); points(1,:)], [points(2,:); points(2,:)],zz,[rs; rs],'EdgeColor','interp');
 set(h, 'LineWidth', 2.5)
-% plot(apex(:,1),apex(:,2),'bd')
+%plot(apex(1,:),apex(2,:),'bd')
 %plot(center(:,1),center(:,2),'ro')
 %quiver(diff_points(2:end-1,1),diff_points(2:end-1,2),radius_vector(:,1),radius_vector(:,2),'g-')
 colormap jet
 colorbar
 % caxis([0 75])
+%plot(points(1,apex),points(2,apex),'ro')
+%scatter(manual_apex(:,1),manual_apex(:,2),[],'g','filled','s')
 hold off
 disp(1/max(rs) + 2.25)
+
 %%
 function center = calc_circle(p1, p2, p3)
     mid1 = (p2+p1) / 2;

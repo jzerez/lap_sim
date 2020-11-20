6c1933d5e71948bccf6a52fb27544db0b163dfde
Naomi Chiu <nchiu@olin.edu>
2020-11-20 01:17:51 -0500

    updated get_points method

--- classes/Track.m
+++ classes/Track.m
@@ -1,6 +1,7 @@
 classdef Track
     properties
-        num_points; % track points 
+        num_points; % number of track points
+        points;     % track points
         curvature;  % curvature of the track at each point
         apex;       % indices of apex points
     end
@@ -14,16 +15,18 @@ classdef Track
             % Parameters: 
             %   track_img: an image of the track to be used, scale must be
             %       specified in scale variable
-            %   apex_points: a .csv file containing the positions of the
+            %   apex_points (optional): a .csv file containing the positions of the
             %       apexes found manually
             % 
             % Returns: 
-            %   self.num_points: 2D array that contains the list of points
+            %   self.num_points: double value representing the number of
+            %       track points
+            %   self.points: 2D array that contains the list of points
             %   	that define the track (each column is a new point, row
             %   	1 is the x-coordinate, row 2 is the y-coordinate)
-            %   self.curvatuer: the curvature at each point along the track
+            %   self.curvature: the curvature at each point along the track
             %   self.apex: list of indexes of points that are at turn
-            %   apexes
+            %       apexes
             
             trace = ~imbinarize(rgb2gray(imread(track_img)), 0.5);
             scale = 0.4;                            % meters per pixel
@@ -37,7 +40,8 @@ classdef Track
             points = zeros(size(reduced));          % create empty matrix
             points(1, :) = reduced(2, :)*scale;     % convert pixel positions to meters and move x-coordinates to first row
             points(2, :) = (-reduced(1, :)+670).*scale;     % convert pixel positions to meters and move y-coordinates to second row
-            self.num_points = points;
+            self.points = points;
+            self.num_points = size(points,2);
             
             diff_points = diff([points(:, end), points], 1, 2);     % calculate the relative location of points
             rs = zeros([1, length(points)]);        % create empty matrix to hold curvature values
@@ -74,28 +78,27 @@ classdef Track
                     apex(i) = min_indx;
                 end
                 
-            else
-               % computationally find apexes
+            else                            % computationally find apexes
                disp("Computational")
                turns = curvature(1,:)>=0.04;  %labels points as being part of a turn (1) or a straight (0)
 
-                i = 1;
-                j = 1;
-                while i<size(curvature,2)-1
-                    init = i;
-                    while turns(i)-turns(i+1)==0        % find continuous turns/straights
-                        i = i+1;
-                        if i == size(curvature,2)-1
-                            break
-                        end
-                    end
-                    if turns(init) == 1     % if not a straight
-                        [curve, index] = max(curvature(:,init:i));     % find global max
-                        apex(j) = init+index-1;             % save global index
-                        j = j+1;
-                    end
-                    i = i+1;
-                end
+               i = 1;
+               j = 1;
+               while i<size(curvature,2)-1
+                   init = i;
+                   while turns(i)-turns(i+1)==0        % find continuous turns/straights
+                       i = i+1;
+                       if i == size(curvature,2)-1
+                           break
+                       end
+                   end
+                   if turns(init) == 1     % if not a straight
+                       [curve, index] = max(curvature(:,init:i));     % find global max
+                       apex(j) = init+index-1;             % save global index
+                       j = j+1;
+                   end
+                   i = i+1;
+               end
             end
             self.apex = apex;    
         end
@@ -104,12 +107,12 @@ classdef Track
             % Plot function to plot a visualization of the track and the
             %   turn apexes
             
-            zz = zeros([2, length(self.num_points)]);
+            zz = zeros([2, self.num_points]);
             figure
             hold on
-            h = surf([self.num_points(1,:); self.num_points(1,:)], [self.num_points(2,:); self.num_points(2,:)],zz,[self.curvature; self.curvature],'EdgeColor','interp');
+            h = surf([self.points(1,:); self.points(1,:)], [self.points(2,:); self.points(2,:)],zz,[self.curvature; self.curvature],'EdgeColor','interp');
             set(h, 'LineWidth', 2.5)
-            plot(self.num_points(1,self.apex),self.num_points(2,self.apex),'bd')
+            plot(self.points(1,self.apex),self.points(2,self.apex),'bd')
             colormap jet
             colorbar
             hold off
@@ -117,7 +120,11 @@ classdef Track
         
         function [points, radii] = get_points(self, apex_index)
             % returns the x,y coordinates for the track points between the
-            % current apex_index and the next apex index. 
+            %   current apex_index and the next apex index. 
+            init = self.apex(apex_index+1);
+            final = self.apex(apex_index+2);
+            points = self.points(:,init:final);
+            radii = 1./self.curvature(init:final)';
         end
     end
     
